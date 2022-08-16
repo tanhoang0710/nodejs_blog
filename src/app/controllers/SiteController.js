3543496864270377765a27db49c6731b192d4cf0
tanhoang0710 <tanhoang0710@gmail.com>
2022-08-16 11:35:40 +0700

    25. [MVC] Model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,12 +1,22 @@
+const Course = require('../models/Course');
+
 class SiteController {
 	// [GET] /
 	index(req, res) {
-		res.render("home");
+		Course.find({}, (err, courses) => {
+			if (!err) {
+				res.json(courses);
+				return;
+			}
+			res.status(400).json({ error: 'ERROR!!!' });
+		});
+
+		//res.render('home');
 	}
 
 	// [GET] /search
 	search(req, res) {
-		res.render("search");
+		res.render('search');
 	}
 }
 
