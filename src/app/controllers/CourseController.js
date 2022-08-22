4e197a7abef7b22e5ad8013eff22fe37fd38e48e
tanhoang0710 <tanhoang0710@gmail.com>
2022-08-22 11:02:40 +0700

    30. [CRUD] Update course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -11,6 +11,22 @@ class CourseController {
 			)
 			.catch(next);
 	}
+
+	create(req, res, next) {
+		res.render('courses/create');
+	}
+
+	store(req, res, next) {
+		const formData = req.body;
+		formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+		const course = new Course(formData);
+		course
+			.save()
+			.then(() => {
+				res.redirect(`/`);
+			})
+			.catch((err) => {});
+	}
 }
 
 module.exports = new CourseController();
