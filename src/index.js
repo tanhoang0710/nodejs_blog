e76181372865b81ca2290aa98765455dbde4e6d6
tanhoang0710 <tanhoang0710@gmail.com>
2022-08-23 10:06:43 +0700

    30. [CRUD] Update course

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
@@ -16,8 +17,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 // HTTP Logger
 app.use(morgan('combined'));
 
+app.use(methodOverride('_method'));
+
 // Template engine
-app.engine('hbs', engine({ extname: '.hbs' }));
+app.engine(
+	'hbs',
+	engine({
+		extname: '.hbs',
+		helpers: {
+			sum: (a, b) => a + b,
+		},
+	})
+);
 app.set('view engine', 'hbs');
 
 // app.set('views', path.join(__dirname, 'resource', 'views')); // chay bang cmd hay terminal deu duoc
